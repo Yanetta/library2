e61dbdb3b93bbfd45443fa2d58df3d5dfc275b1b
Yanetta <revenkot81@gmail.com>
2018-12-19 18:19:22 +0200

    changed Lybrary2 line28

--- src/main/java/library2/Library2.java
+++ src/main/java/library2/Library2.java
@@ -6,25 +6,26 @@ public class Library2 {
     public Library2(){
     }
     public void getFinishedBooks (HashMap<String, Boolean> lib){
-            if (lib.size() < 1) {
-                System.out.println("HashMap is empty");
-            }
-            else {
-                for (String book : lib.keySet()) {
-                    if (lib.get(book) == true) {
-                        System.out.println(book);
-                    }
+        if (lib.size() < 1) {
+            System.out.println("HashMap is empty");
+        }
+        else {
+            for (String book : lib.keySet()) {
+                if (lib.get(book) == true) {
+                    System.out.println(book);
                 }
             }
         }
+    }
     public static void main(String[] args) {
-            HashMap<String, Boolean> myBooks = new HashMap<String, Boolean>();
-            myBooks.put("Road Down The Funnel", true);
-            myBooks.put("Rat: A Biology", false);
-            myBooks.put("TimeIn", true);
-            myBooks.put("3D Food Printing", false);
-            Library2 myLibrary = new Library2();
-            myLibrary.getFinishedBooks(myBooks);
+        HashMap<String, Boolean> myBooks = new HashMap<String, Boolean>();
+        myBooks.put("Road Down The Funnel", true);
+        myBooks.put("Rat: A Biology", false);
+        myBooks.put("TimeIn", true);
+        myBooks.put("3D Food Printing", false);
+        Library2 myLibrary = new Library2();
+        myLibrary.getFinishedBooks(myBooks);
+        System.out.println("good books");
         }
 }
 
